cedadca9ea39a0230197fdb422589eed31069921
Esther Lucena <stena.developer@gmail.com>
2023-10-02 19:20:47 -0300

    att.

--- exerMain.js
+++ exerMain.js
@@ -1,27 +1,27 @@
 // 1 Filter
 
-const numeros= [2,3,4,5,6,7,8];
+const numeros = [2, 3, 4, 5, 6, 7, 8];
 const pares = numeros.filter((numero) => numero % 2 === 0);
 console.log(pares);
 
 // 2 Map
 
-const nomes= ['Esther','Maria','Jana','Vic'];
-const nomesUpper = nomes.map((nome)=> nome.toUpperCase());
-console.log(nomesUpper); 
+const nomes = ['Esther', 'Maria', 'Jana', 'Vic'];
+const nomesUpper = nomes.map((nome) => nome.toUpperCase());
+console.log(nomesUpper);
 
 // 3 ForEach
 
-var names = ['Esther','Maria','Julia','Jana','Vic','Lari'];
-names.forEach(function(item, indice, array){
+var names = ['Esther', 'Maria', 'Julia', 'Jana', 'Vic', 'Lari'];
+names.forEach(function (item) {
     console.log(`Olá ${item}!`);
 });
 
 // 4 - 5 Filter e Map ( strings )
 
-const strings1= ['Maça' , 'Banana' , 'Manga' , 'Kiwi'];
-const letraM = strings1.filter((letra) => letra.charAt(0)=='M');
-const fruitsUpper = letraM.map((name)=> name.toUpperCase());
+const strings1 = ['Maça', 'Banana', 'Manga', 'Kiwi'];
+const letraM = strings1.filter((letra) => letra.charAt(0) === 'M' || letra.charAt(0) === 'm');
+const fruitsUpper = letraM.map((name) => name.toUpperCase());
 console.log(`Essas são as frutas: ${strings1}`);
 console.log(`Essas sao as frutas que começam com a letra M: ${letraM}`);
 console.log(fruitsUpper);
@@ -34,16 +34,13 @@ console.log(fruitsUpper);
 // const dobrados = numeros.map((numero)=> numero * 2);
 
 
-const notasAluno = [5,7,9];
-const soma = notasAluno.reduce((cont, valor) => cont + valor, 0);
+const notasAluno = [5, 7, 9];
+const soma = notasAluno.reduce((soma, notinha) => soma + notinha, 0);
 const media = soma / notasAluno.length;
-console.log(media); 
-
-const alunosAprovados = notasAluno.filter((note) => note >= 7) ;
-console.log(alunosAprovados);
+console.log(media);
 
 if (media >= 7) {
     console.log('Aprovado!');
-}  else {
+} else {
     console.log('Reprovado!');
 }
